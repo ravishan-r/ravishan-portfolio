1cd5d1ebadde0de53c14cd3f56beb2e010757e71
Ravishan Rathnayake <ravishanrathnayakeprs@gmail.com>
2026-07-31 00:06:31 +0530

    chore: initialize React portfolio with Vite and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+     <main>
+      <h1>Ravishan Portfolio</h1>
+    </main>
+  )
+}
+
+export default App
