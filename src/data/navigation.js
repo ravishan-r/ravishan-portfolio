6ae74ee0cbf8fd2a54910fc71bbdcc4e9a4d2a73
Ravishan Rathnayake <ravishanrathnayakeprs@gmail.com>
2026-08-08 18:32:11 +0530

    refactor(nav): remove unfinished blog link

--- src/data/navigation.js
+++ src/data/navigation.js
@@ -4,6 +4,5 @@ export const navigationLinks = [
   { name: "Skills", href: "#skills" },
   { name: "Projects", href: "#projects" },
   { name: "Journey", href: "#journey" },
-  { name: "Blog", href: "#blog" },
   { name: "Contact", href: "#contact" },
 ];
